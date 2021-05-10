73ee3dce8e6fdf2214d23d83b28c0eaf73f810a9
ari0129 <82903510+ari0129@users.noreply.github.com>
2021-05-10 14:34:58 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,30 +2,32 @@
  * At anytime, you can right-click on a block and select "help" for more info on the specific block and how it can be used.
  */
 // To comment in your JS, use the double /
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     game.over(true)
 })
 tiles.setTilemap(tilemap`level1`)
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 3 3 3 3 3 . . . . . . . 
-    . . . . 3 3 3 3 3 . . . . . . . 
-    . . . . 3 3 3 3 3 . . . . . . . 
-    . . . . 3 3 3 3 3 . . . . . . . 
-    . . . . 3 3 3 3 3 . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+game.splash("help me find my way to the carrot", "")
 tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
 mySprite.setVelocity(70, 70)
 controller.moveSprite(mySprite)
-mySprite.startEffect(effects.fire)
+mySprite.startEffect(effects.trail)
 scene.cameraFollowSprite(mySprite)
+music.baDing.play()
